778ffa27a27cf43f3c665190ac34b7fb8f754d15
jannabiforever <jungini1226@gmail.com>
2025-05-30 11:58:55 +0900

    impl: Base Data Structure

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,6 +7,32 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+		interface RootProject {
+			id: string;
+			name: string;
+			goal: string;
+			childProjectIds: string[];
+		}
+
+		interface ChildProject {
+			id: string;
+			name: string;
+			goal: string;
+			rootProjectId: string;
+			tasks: string[];
+		}
+
+		interface Day {
+			date: string;
+			// [startTime, endTime, Chunk]
+			chunks: [number, number, Chunk][];
+		}
+
+		interface Chunk {
+			childProjectId: string;
+			// Aggregated to be one of childProject's tasks.
+			tasks: string[];
+		}
 	}
 }
 
