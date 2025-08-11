5248f738b1a3365b2e2b0f1f87ad65eb625312bc
jannabiforever <jungini1226@gmail.com>
2025-08-11 17:45:24 +0900

    chore: App.Error types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,12 @@
 import type { Session, User } from '@supabase/supabase-js';
 import type { SupabaseService } from '$lib/infra/supabase/layer.server';
 import type { Layer } from 'effect';
-import type { TaggedErrorClass } from 'effect/Schema';
 
 declare global {
 	namespace App {
-		interface Error extends TaggedErrorClass {
+		interface Error {
 			readonly _tag: string;
+			readonly message: string;
 		}
 		interface Locals {
 			supabase: Layer.Layer<SupabaseService, never, never>;
