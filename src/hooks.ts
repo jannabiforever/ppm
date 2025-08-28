1a19fa407633d16f293cb3da9273ec4578f1d7a3
kodingk <kodingk3153@gmail.com>
2025-08-28 17:01:08 +0900

    feat: DateTime.Utc transporter

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,26 +1,27 @@
 import type { Transport, Transporter } from '@sveltejs/kit';
 import * as Option from 'effect/Option';
 import * as Either from 'effect/Either';
+import * as DateTime from 'effect/DateTime'
 
 // ------------------------------------------------------------
 // Option 트랜스포터
 // ------------------------------------------------------------
 
 type OptionObject<T> =
-	| {
-			_tag: 'None';
-	  }
-	| {
-			_tag: 'Some';
-			value: T;
-	  };
+  | {
+    _tag: 'None';
+  }
+  | {
+    _tag: 'Some';
+    value: T;
+  };
 
 const OptionTransporter: Transporter = {
-	encode: (value) => {
-		if (!Option.isOption(value)) return false;
-		return Option.isSome(value) ? { _tag: 'Some', value: value.value } : { _tag: 'None' };
-	},
-	decode: (oo: OptionObject<unknown>) => (oo._tag == 'Some' ? Option.some(oo.value) : Option.none())
+  encode: (value) => {
+    if (!Option.isOption(value)) return false;
+    return Option.isSome(value) ? { _tag: 'Some', value: value.value } : { _tag: 'None' };
+  },
+  decode: (oo: OptionObject<unknown>) => (oo._tag == 'Some' ? Option.some(oo.value) : Option.none())
 };
 
 // ------------------------------------------------------------
@@ -28,29 +29,38 @@ const OptionTransporter: Transporter = {
 // ------------------------------------------------------------
 
 type EitherObject<E, A> =
-	| {
-			_tag: 'Left';
-			value: E;
-	  }
-	| {
-			_tag: 'Right';
-			value: A;
-	  };
+  | {
+    _tag: 'Left';
+    value: E;
+  }
+  | {
+    _tag: 'Right';
+    value: A;
+  };
 
 const EitherTransporter: Transporter = {
-	encode: (value) => {
-		if (!Either.isEither(value)) return false;
-		return Either.isLeft(value)
-			? { _tag: 'Left', value: value.left }
-			: { _tag: 'Right', value: value.right };
-	},
-	decode: (eo: EitherObject<unknown, unknown>) =>
-		eo._tag == 'Left' ? Either.left(eo.value) : Either.right(eo.value)
+  encode: (value) => {
+    if (!Either.isEither(value)) return false;
+    return Either.isLeft(value)
+      ? { _tag: 'Left', value: value.left }
+      : { _tag: 'Right', value: value.right };
+  },
+  decode: (eo: EitherObject<unknown, unknown>) =>
+    eo._tag == 'Left' ? Either.left(eo.value) : Either.right(eo.value)
 };
+
+const DateTimeUtcTransporter: Transporter = {
+  encode: (value) => {
+    if (!DateTime.isUtc(value)) return false;
+    return DateTime.formatIsoDateUtc(value);
+  },
+  decode: (utc: string) => DateTime.unsafeMake(utc)
+}
 
 // TODO: 에러 트랜스포터
 // TODO: 현재 구현으로는 Option.Option<Either.Either<E, A>> 같은 네스팅 되어 있는 경우를 처리할 수 없음.
 export const transport: Transport = {
-	Option: OptionTransporter,
-	Either: EitherTransporter
+  Option: OptionTransporter,
+  Either: EitherTransporter,
+  DateTimeUtc: DateTimeUtcTransporter
 };
